2a2177b2c411d4117d4b1ff3eaa177f2e87c3dea
Kevar Halstead <kevarhalstead@yahoo.com>
2019-10-21 22:09:24 -0500

    initial js file

---
+++ tic-tac-toe.js
@@ -0,0 +1,21 @@
+// "use strict";
+
+
+
+// let boxes = document.querySelector('div').className = "square";
+
+
+// boxes.forEach(function(elem, index, list) {
+// 	elem.addEventListener('onload', function(e) 
+// {
+// 	e.target.classList.add('status');
+// })
+// })
+
+window.onload = function(){
+
+var square = document.getElementById("board");
+		board.classList.add('square');
+		console.log(square);
+
+}
