676bcacced66ac0ee5bde84852c7df9f60a548c5
Kevar Halstead <kevarhalstead@yahoo.com>
2019-11-22 17:22:40 +0000

    Task 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,54 @@
 window.addEventListener('load', () => {
   const squares = document.querySelectorAll('#board div');
-
-  for (let x = 0; x < squares.length; x++) {
-    squares[x].classList.add('square');
+  let playerX = true;
+  
+  for (let y = 0; y < squares.length; y++) {
+    squares[y].classList.add('square');
     console.log(squares);
     // Now Add an event listener when a square is clicked
-   // squares[x].addEventListener('click', () => {
+   // squares[y].addEventListener('click', () => {
       // write code to add X or O to the square clicked
-  }
+   
+   
+   squares[y].addEventListener('click', () => {
+    console.log(y);
+   
+    
+    
+    console.log(squares);
+    
+    
+    
+    if (playerX == true) {
+      squares[y].innerHTML = 'X';
+      squares[y].classList.add('X');
+      // playerX = false;
+    } else {
+      
+      squares[y].innerHTML = 'O';
+      squares[y].classList.add('O');
+      // playerX = true;
+    }
+     playerX = !playerX; 
+    
+    // //Use a function to keep track of clicks
+    // function clickTrack(){
+    //   const clickTracker = [];
+    // };
+   });
+      
+  }   
+  
+  
+// let X = document.createTextNode('X');
+// let O = document.createTextNode('O');
+
+// square.append(X);
+// document.body.append(square.X);
+
+// square.append(O);
+// document.body.append(square.O);
+
+  // for (let X = 0; X);
   
 });
